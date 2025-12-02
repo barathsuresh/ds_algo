395650088c5405145690b017d5092a1ce26aecab
barath_suresh <barathsuresh3@gmail.com>
2025-12-02 00:00:14 -0700

    modified two sum II

--- Blind_75/2_two_pointers/two_sum.py
+++ Blind_75/2_two_pointers/two_sum.py
@@ -1,25 +1,20 @@
-"""
-Two Sum II - Input Array Is Sorted
-Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order, find two numbers such that they add up to a specific target number.
-Let these two numbers be numbers[index1] and numbers[index2] where 1 <= index1 < index2 <= numbers.length.
-Return the indices of the two numbers, index1 and index2, added by one as an array [index1, index2] of length 2.
-The tests are generated such that there is exactly one solution. You may not use the same element twice.
-You can return the answer in any order.
-Example 1:
-Input: numbers = [2,7,11,15], target = 9
-Output: [1,2]
-Explanation: The sum of 2 and 7 is 9. Therefore, index1 = 1, index2 = 2. We return [1, 2].
-Example 2:
-Input: numbers = [2,3,4], target = 6
-Output: [1,3]
-Explanation: The sum of 2 and 4 is 6. Therefore, index1 = 1, index2 = 3. We return [1, 3].
-"""
-
 from typing import List
-
-
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        """
+        Problem: Find two numbers in a sorted array that add up to target.
+        Return their 1-based indices.
+        
+        The solution must use only constant extra space.
+
+        Example 1:
+        Input: numbers = [2,7,11,15], target = 9
+        Output: [1,2] (2 + 7 = 9)
+
+        Example 2:
+        Input: numbers = [2,3,4], target = 6
+        Output: [1,3] (2 + 4 = 6)
+        """
         left = 0
         right = len(numbers) - 1
         while left < right:
