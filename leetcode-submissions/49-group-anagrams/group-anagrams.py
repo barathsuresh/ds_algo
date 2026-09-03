aa9ff8ea0c29409d48a91ac06756ef19cd5770ed
Barath Suresh <barathsuresh3@gmail.com>
2026-09-03 14:40:58 -0700

    Time: 18 ms (21.78%) | Memory: 24.1 MB (9.62%) - LeetSync

--- leetcode-submissions/49-group-anagrams/group-anagrams.py
+++ leetcode-submissions/49-group-anagrams/group-anagrams.py
@@ -1,14 +1,13 @@
-
 from typing import List
 from collections import defaultdict
 
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        word_map = defaultdict(list)
-        res = []
-        for i in strs:
-            s = "".join(sorted(i))
-            word_map[s].append(i)
-        for index, (key, value) in enumerate(word_map.items()):
-            res.append(value)
-        return res
+        result = defaultdict(list)
+        # we are creating like a key value such that. the in alphabets and all snd storing the values as we encounter a match
+        for s in strs:
+            count = [0] * 26
+            for c in s:
+                count[ord(c) - ord("a")] += 1
+            result[tuple(count)].append(s)
+        return list(result.values()) # Takes of O(m*n) complexity becuz m is the number of. items and n is avg characters for the strings
