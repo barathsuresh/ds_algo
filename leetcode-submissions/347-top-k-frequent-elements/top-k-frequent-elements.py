34580283fc36cc407053dc547e7f54158a1179d0
Barath Suresh <barathsuresh3@gmail.com>
2026-09-18 03:30:26 -0700

    Time: 8 ms (33.62%) | Memory: 24.9 MB (12.48%) - LeetSync

--- leetcode-submissions/347-top-k-frequent-elements/top-k-frequent-elements.py
+++ leetcode-submissions/347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -2,13 +2,47 @@ import heapq
 from collections import defaultdict
 class Solution:
     def topKFrequent(self, nums: list[int], k: int) -> list[int]:
-        count_freq = defaultdict(int)
-        res = []
-        for i in nums:
-            count_freq[i] += 1
-        for x,v in count_freq.items():
-            heapq.heappush(res,(v,x))
-            if len(res) > k:
-                heapq.heappop(res)
+        # heap implementation 
+        # count_freq = defaultdict(int)
+        # res = []
+        # for i in nums:
+        #     count_freq[i] += 1
+        # for x,v in count_freq.items():
+        #     heapq.heappush(res,(v,x))
+        #     if len(res) > k:
+        #         heapq.heappop(res)
         
-        return [val for freq, val in res]
+        # return [val for freq, val in res]
+
+        count = defaultdict(int)
+        freq = [[] for i in range(len(nums) + 1)]
+
+        for n in nums:
+            count[n] += 1
+        
+        for n, c in count.items():
+            freq[c].append(n)
+        
+        res = []
+        for i in range(len(freq) - 1, 0, -1):
+            for n in freq[i]:
+                res.append(n)
+                if len(res) == k:
+                    return res
+
+    def topKFrequentBucketSort(self, nums: list[int], k: int) -> list[int]:
+        count = defaultdict(int)
+        freq = [[] for i in range(len(nums) + 1)]
+
+        for n in nums:
+            count[n] += 1
+        
+        for n, c in count.items():
+            freq[c].append(n)
+        
+        res = []
+        for i in range(len(freq) - 1, 0, -1):
+            for n in freq[i]:
+                res.append(i)
+                if len(res) == k:
+                    return res
