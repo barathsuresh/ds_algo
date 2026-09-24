630ccf667f2049bb4fca4e14fa013d93c034e037
Barath Suresh <barathsuresh3@gmail.com>
2026-09-24 03:59:12 -0700

    Time: 18 ms (76.57%) | Memory: 25.6 MB (54.20%) - LeetSync

--- leetcode-submissions/238-product-of-array-except-self/product-of-array-except-self.py
+++ leetcode-submissions/238-product-of-array-except-self/product-of-array-except-self.py
@@ -3,12 +3,12 @@ class Solution:
         n = len(nums)
         res = [1]*len(nums)
 
-        pre = 1
+        pre = 1 # first calclute the prefix calculations
         for i in range(len(nums)):
             res[i] = pre
             pre *= nums[i]
         
-        post = 1
+        post = 1 # and from backwards do the same such that we get the product of array except self
         for i in range(n-1,-1,-1):
             res[i] *= post
             post *= nums[i]
